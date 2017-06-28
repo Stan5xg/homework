45367d51c74936a56bbca0f0589af04469d214be
Stan5xg <sudostan@gmail.com>
2017-06-28 16:09:14 +0300

    Simple unit modified

--- Homework/src/testing/SimpleUnit.java
+++ Homework/src/testing/SimpleUnit.java
@@ -34,4 +34,19 @@ public class SimpleUnit {
 		return notEquals;
 	}
 	
+	public static boolean assertEquals(boolean actual, boolean expected) {
+		boolean equals = actual == expected;
+		if (!equals) {
+			System.err.println("Test failed: expected " + expected + ", but actual " + actual);
+		}
+		return equals;
+	}
+	
+	public static boolean assertNotEquals(boolean actual, boolean expected) {
+		boolean notEquals = actual != expected;
+		if (!notEquals) {
+			System.err.println("Test failed: expected NOT " + expected + ", but actual " + actual);
+		}
+		return notEquals;
+	}
 }
