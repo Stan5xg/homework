edac0385e69229fde911139b94b25690a04e55db
Stan5xg <sudostan@gmail.com>
2017-07-11 17:25:20 +0300

    work-in-progress

---
+++ Homework/src/lek8/UseStringArrayList.java
@@ -0,0 +1,40 @@
+package lek8;
+
+import mytestframework.Asserts;
+
+public class UseStringArrayList {
+
+	public static void main(String[] args) {
+		
+		StringArrayList list = new StringArrayList();
+		
+		list.add("one");
+		
+		System.out.println(list);
+		
+		list.add("two");
+		
+		System.out.println(list);
+
+		list.remove(0);
+		list.remove(0);
+		
+		System.out.println(list);
+		
+		list.add("three");
+		
+		System.out.println(list);
+		
+		list.add("four");
+		list.add("five");
+		
+		System.out.println(list);
+		
+		list.remove(2);
+		
+		System.out.println(list);
+		
+		System.out.println(Asserts.assertEquals(list.get(0), "four"));
+	}
+	
+}
