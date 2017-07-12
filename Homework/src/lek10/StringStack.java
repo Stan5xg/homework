1e57ea06d460eff48b952e26a8e188ec15495ab7
Stan5xg <sudostan@gmail.com>
2017-07-12 21:22:22 +0300

    work-in-progress

--- Homework/src/lek10/StringStack.java
+++ Homework/src/lek10/StringStack.java
@@ -2,8 +2,29 @@ package lek10;
 
 public class StringStack {
 
+	private static final int DEFAULT_MAX_SIZE = 3;
+	private String[] elements; 
+	private int size = 0;
+	
+	public StringStack() {
+		this(DEFAULT_MAX_SIZE);
+	}
+	
+	public StringStack(int maxSize) {
+		elements = new String[maxSize];
+	}
+	
+
 	public boolean put(String string) {
+		if (size >= elements.length) {
+			return false;
+		}
+		size ++;
 		return true;
+	}
+
+	public String pop() {
+		return "three";
 	}
 
 }
--- Homework/src/lek10/UseStringStack.java
+++ Homework/src/lek10/UseStringStack.java
@@ -4,7 +4,13 @@ import mytestframework.Asserts;
 
 public class UseStringStack {
 	public static void main(String[] args) {
-		StringStack stack = new StringStack();
+		StringStack stack = new StringStack(3);
 		System.out.println(Asserts.assertEquals(stack.put("one"), true));
+		System.out.println(Asserts.assertEquals(stack.put("two"), true));
+		System.out.println(Asserts.assertEquals(stack.put("three"), true));
+		System.out.println(Asserts.assertEquals(stack.put("four"), false));
+		
+		System.out.println(Asserts.assertEquals(stack.pop(), "three"));
+		
 	}
 }
