75831f269b287912a9916f6bf41e1c7362141604
Stan5xg <sudostan@gmail.com>
2017-08-12 10:36:30 +0300

    BinarySearch task is done

--- Homework/src/lek21/BinarySearch.java
+++ Homework/src/lek21/BinarySearch.java
@@ -3,8 +3,23 @@ package lek21;
 import java.util.Collection;
 
 public class BinarySearch {
-	public static <T> int binarySearch(Collection<? extends Comparable<T>> c, T key) {
-		return 0;
-	}
+	public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        int mid = -1;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        int insertionPoint;
+        if (key > a[mid]) {
+        	insertionPoint = mid + 1;
+        } else {
+        	insertionPoint = mid;
+        }
+        return - (insertionPoint + 1);
+    }
 
 }
--- Homework/src/lek21/UseBinarySearch.java
+++ Homework/src/lek21/UseBinarySearch.java
@@ -1,31 +1,39 @@
 package lek21;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 
 public class UseBinarySearch {
 
+	private static final int TEST_ARRAY_MAX_SIZE = 30;
+
 	public static void main(String[] args) {
-		List<Integer> list = new ArrayList<>();
-		fill(list, 10);
-		System.out.println(list);
-		
-		Collections.sort(list);
-		
-		System.out.println(list);
-		
-		System.out.println(BinarySearch.binarySearch(list, list.get(4)));;
-		
+		testAgaintsLibraryBinSearch(20);
 	}
 
-	private static void fill(List<Integer> list, int size) {
+	private static void testAgaintsLibraryBinSearch(int reps) {
 		Random r = new Random();
-		
-		while(list.size() < size) {
-			list.add(r.nextInt(size));
+		for (int i = 0; i < reps; i++) {
+			int[] a = new int[TEST_ARRAY_MAX_SIZE];
+			fill(a);
+			Arrays.sort(a);
+			int key = r.nextInt(a.length);
+			int testingResult = BinarySearch.indexOf(a, key);
+			int referenceResult = Arrays.binarySearch(a, key);
+			
+			boolean passed = testingResult == referenceResult;
+			if (passed) {
+				System.out.println(passed);
+			} else {
+				System.err.println(passed + " " + Arrays.toString(a) + " >" + key + " got " + testingResult + " need " + referenceResult);
+			}
+		}		
+	}
+
+	private static void fill(int[] a) {
+		Random r = new Random();
+		for(int i = 0; i < a.length; i++) {
+			a[i] = r.nextInt(a.length);
 		}
-		
 	}
 }
