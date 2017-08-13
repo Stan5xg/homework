39a6b0a93d4d1b65db8602086f7e3bc62a769ce2
Stan5xg <sudostan@gmail.com>
2017-08-13 10:04:07 +0300

    work-in-progress

--- Homework/src/lek21/A.java
+++ Homework/src/lek21/A.java
@@ -6,6 +6,10 @@ class A implements Comparable<A> {
 	static Random r = new Random(42);
 	int x = r.nextInt(10);
 	
+	public static void resetRandom() {
+		r = new Random(42);
+	}
+	
 	@Override
 	public String toString() {
 		return Integer.toString(x);
--- Homework/src/lek21/SortClassExample.java
+++ Homework/src/lek21/SortClassExample.java
@@ -13,23 +13,30 @@ public class SortClassExample {
 		
 		fill(a);
 		
+		A[] a1 = new A[10];
+		A.resetRandom();
+		
+		fill(a1);
+		
 		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(a1));
 		
-		Arrays.sort(a);
 		
-		System.out.println(Arrays.toString(a));
-		
-		List<A> list = new LinkedList<>();
-		
-		for (int i = 0; i < 10; i++) {
-			list.add(new A());
-		}
-		
-		System.out.println(list);
-		
-		Collections.sort(list);
-		
-		System.out.println(list);
+//		Arrays.sort(a);
+//		
+//		System.out.println(Arrays.toString(a));
+//		
+//		List<A> list = new LinkedList<>();
+//		
+//		for (int i = 0; i < 10; i++) {
+//			list.add(new A());
+//		}
+//		
+//		System.out.println(list);
+//		
+//		Collections.sort(list);
+//		
+//		System.out.println(list);
 		
 	}
 
